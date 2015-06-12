2c3a241aac3b86f218a0b5dce812ae7fd44deaf4
Troy S. Wood <tsw@outlook.com>
2015-06-11 23:22:15 -0700

    cleaned up code format

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-// Code goes here
-
-
 
 var data = [
-   { name: 'Ryan', points: 14},
-   { name: 'Colin', points: 7},
-   { name: 'Tania', points: 9},
-   { name: 'Nick', points: 21}
+  { name: 'Ryan', points: 14},
+  { name: 'Colin', points: 7},
+  { name: 'Tania', points: 9},
+  { name: 'Nick', points: 21}
  ];
 
  var leaderboard2 = function(data) {
-   return data.sort(function(a,b){return b.points-a.points;})
+  return data.sort(function(a,b){return b.points-a.points;})
  }
 
  function liCreate(name,points) {
-   var li = $('<li>'+name+'</li>');
-   li.append('<span>'+points+'</span>');
-
+  var li = $('<li>'+name+'</li>');
+  li.append('<span>'+points+'</span>');
  }
 
  $(document).ready(function() {
-   var sorted = leaderboard2(data);
-   for (var i=0; i<sorted.length; i++) {
-     $('body').append('<div><p>'+sorted[i].name+' '+sorted[i].points+'</p></div>')
-   }
-   
+  var sorted = leaderboard2(data);
+  for (var i=0; i<sorted.length; i++) {
+    $('body').append('<div><p>'+sorted[i].name+' '+sorted[i].points+'</p></div>')
+  }
  })
 
